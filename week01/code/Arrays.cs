bd81d65e3b0a25c1538f4c45f97d4eb4efb73e0c
Jctman5 <Jctman5@gmail.com>
2026-03-09 20:39:15 -0400

    CSE 212 homework

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 public static class Arrays
 {
     /// <summary>
@@ -12,8 +14,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        
+        // 1. Create new double array that takes length as a parameter.
+        // 2. Use a loop to generate each multiple.
+        // 3. Store each multiple in the index of the multiples array.
+        // 4. Return the array.
 
-        return []; // replace this return statement with your own
+        double[] multiples = new double[length];
+        for(int i = 0; i<length; i++){
+            multiples[i] = number * (i+1);
+        }
+        
+        
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +42,18 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // 1. Create a loop that repeates for "amount" number of times.
+        // 2. Get the last value in the list and assign it to a variable.
+        // 3. Remove the last value in the list.
+        // 4. Insert that variable in the beginning of the list.
+
+        for(int i = 0; i<amount; i++)
+        {
+            int lastValue = data[data.Count -1];
+            data.RemoveAt(data.Count - 1);
+            data.Insert(0, lastValue);
+        }
+
     }
 }
